87b02d2e5d4f7fd2ca1b40cab152a0ba97ea3ec5
German De Francesco <germandefrancesco@hotmail.com>
2022-04-11 20:02:30 -0300

    implemented practice 2 exercise 2

--- ProgrammingIII/src/Module2/PracticeImplementations.java
+++ ProgrammingIII/src/Module2/PracticeImplementations.java
@@ -1,5 +1,7 @@
 package Module2;
 
+import java.util.Iterator;
+
 public class PracticeImplementations {
 
     public static boolean arrayIsOrdered(int[] array, int position){
@@ -10,6 +12,29 @@ public class PracticeImplementations {
         }
         else{
             return false;
+        }
+    }
+
+    public static boolean valueExistsInList(Iterator<Integer> iterator, int value){
+        if (iterator.hasNext()){
+            if(iterator.next().equals(value)){
+                return true;
+            }
+            else{
+                return valueExistsInList(iterator, value);
+            }
+        }
+        return false;
+    }
+
+    public static boolean valueExistsInOrderedArray(int[] array, int value, int position){
+        if(position < 0 || position >= array.length - 1)
+            return false;
+        if(!valueExistsInOrderedArray(array, value, position + 1)){
+            return array[position] == value;
+        }
+        else{
+            return true;
         }
     }
 
--- ProgrammingIII/src/Module2/Program.java
+++ ProgrammingIII/src/Module2/Program.java
@@ -1,14 +1,22 @@
 package Module2;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class Program {
 
 	public static void main(String[] args) {
 		int[] arrayOrdered = new int[] { 1, 2, 3, 4, 5 };
 		int[] arrayNotOrdered = new int[] { 1, 2, 4, 3, 5 };
-		boolean isOrdered1 = PracticeImplementations.arrayIsOrdered(arrayOrdered, 0);
-		boolean isOrdered2 = PracticeImplementations.arrayIsOrdered(arrayNotOrdered, 0);
-		System.out.println(isOrdered1);
-		System.out.println(isOrdered2);
+		LinkedList<Integer> list = new LinkedList(Arrays.asList(1, 2, 3, 4, 5));
+		// Exercise 1
+		System.out.println(PracticeImplementations.arrayIsOrdered(arrayOrdered, 0));
+		System.out.println(PracticeImplementations.arrayIsOrdered(arrayNotOrdered, 0));
+		// Exercise 2
+		System.out.println(PracticeImplementations.valueExistsInList(list.iterator(), 4));
+		System.out.println(PracticeImplementations.valueExistsInList(list.iterator(), 6));
+		System.out.println(PracticeImplementations.valueExistsInOrderedArray(arrayOrdered, 4, 0));
+		System.out.println(PracticeImplementations.valueExistsInOrderedArray(arrayOrdered, 6, 0));
 	}
 
 }
