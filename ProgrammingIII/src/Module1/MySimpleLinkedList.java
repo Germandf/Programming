da79575bd7c2be403544088d3667164cf787320b
German De Francesco <germandefrancesco@hotmail.com>
2022-04-21 18:46:03 -0300

    implemented practice 1 exercise 1

---
+++ ProgrammingIII/src/Module1/MySimpleLinkedList.java
@@ -0,0 +1,70 @@
+package Module1;
+
+import java.util.Iterator;
+
+public class MySimpleLinkedList<T> implements Iterable<T> {
+
+    private Node<T> first;
+    private int size;
+
+    public MySimpleLinkedList() {
+        this.first = null;
+    }
+
+    public void insertFront(T info) {
+        Node<T> tmp = new Node<T>(info,null);
+        tmp.setNext(this.first);
+        this.first = tmp;
+        this.size++;
+    }
+
+    public T extractFront() {
+        Node<T> tmp = this.first;
+        if(tmp == null)
+            return null;
+        this.first = this.first.getNext();
+        this.size--;
+        return tmp.getInfo();
+    }
+
+    public boolean isEmpty() {
+        return this.first == null;
+    }
+
+    public T get(int index) {
+        if (this.first == null)
+            return null;
+        var tmp = this.first;
+        for(int i = 0; i < index; i++){
+            if(tmp.getNext() == null)
+                return null;
+            tmp = tmp.getNext();
+        }
+        return tmp.getInfo();
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public String toString() {
+        if (this.first == null)
+            return "";
+        var string = "";
+        var tmp = this.first;
+        for(int i = 0; i < this.size; i++){
+            string += tmp.getInfo().toString();
+            tmp = tmp.getNext();
+            if(tmp != null)
+                string += ", ";
+        }
+        return string;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new MyIterator<T>(this.first);
+    }
+
+}
