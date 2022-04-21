26d2c654560bbd1a1627ad3a3ca28e74ac375c46
German De Francesco <germandefrancesco@hotmail.com>
2022-04-21 18:55:04 -0300

    implemented practice 1 exercise 3

---
+++ ProgrammingIII/src/Module1/Pile.java
@@ -0,0 +1,33 @@
+package Module1;
+
+public class Pile<T> {
+
+    MySimpleLinkedList<T> list;
+
+    public Pile() {
+        this.list = new MySimpleLinkedList<T>();
+    }
+
+    public void push(T o){
+        list.insertFront(o);
+    }
+
+    public T pop(){
+        return list.extractFront();
+    }
+
+    public T top(){
+        var tmp = list.extractFront();
+        list.insertFront(tmp);
+        return tmp;
+    }
+
+    public void reverse(){
+        var size = list.size();
+        var newList = new MySimpleLinkedList<T>();
+        for(int i = 0; i < size; i++)
+            newList.insertFront(this.list.extractFront());
+        this.list = newList;
+    }
+
+}
