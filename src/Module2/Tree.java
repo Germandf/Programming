9d21ecdc41b548b8694e925ea5c186c84bd87f2b
German De Francesco <germandefrancesco@hotmail.com>
2022-05-21 16:45:25 -0300

    practice 2 exercise 7 most methods implemented

--- src/Module2/Tree.java
+++ src/Module2/Tree.java
@@ -1,5 +1,7 @@
 package Module2;
 
+import java.util.ArrayList;
+
 public class Tree {
 
     private Integer value;
@@ -12,6 +14,17 @@ public class Tree {
         this.right = null;
     }
 
+    // O(1)
+    public int getRoot(){
+        return value;
+    }
+
+    // O(1)
+    public boolean isEmpty(){
+        return value == null;
+    }
+
+    // O(h)
     public void add(Integer newValue) {
         if (this.value == null)
             this.value = newValue;
@@ -29,5 +42,105 @@ public class Tree {
             }
         }
     }
+
+    // O(n)
+    public int getHeight(){
+        if(left == null && right == null)
+            return 0;
+        var leftCount = 0;
+        var rightCount = 0;
+        if(left != null)
+            leftCount = left.getHeight() + 1;
+        if(right != null)
+            rightCount = right.getHeight() + 1;
+        return Math.max(leftCount, rightCount);
+    }
+
+    // O(n)
+    public ArrayList<Integer> getLongestBranch(){
+        if(left == null && right == null)
+            return new ArrayList<Integer>(value);
+        var leftList = new ArrayList<Integer>();
+        var rightList = new ArrayList<Integer>();
+        if(left != null){
+            leftList.add(left.value);
+            leftList.addAll(left.getLongestBranch());
+        }
+        if(right != null){
+            rightList.add(right.value);
+            rightList.addAll(right.getLongestBranch());
+        }
+        if(leftList.size() > rightList.size())
+            return leftList;
+        else
+            return rightList;
+    }
+
+    // O(n)
+    public ArrayList<Integer> getLeaves(){
+        if(left == null && right == null)
+            return new ArrayList<Integer>(value);
+        var leaves = new ArrayList<Integer>();
+        if(left != null)
+            leaves.addAll(left.getLeaves());
+        if(right != null)
+            leaves.addAll(right.getLeaves());
+        return leaves;
+    }
+
+    // O(n)
+    public boolean hasElement(int element){
+        if(value == element)
+            return true;
+        var found = false;
+        if(left != null)
+            found = left.hasElement(element);
+        if(right != null && !found)
+            found = right.hasElement(element);
+        return found;
+    }
+
+    // O(h)
+    public boolean hasElementBST(int element){
+        if(value == element)
+            return true;
+        var found = false;
+        if(value > element && left != null)
+            found = left.hasElementBST(element);
+        if(value < element && right != null && !found)
+            found = right.hasElementBST(element);
+        return found;
+    }
+
+    // O(n)
+    public int getMax(){
+        if(left == null && right == null)
+            return value;
+        var currentMax = value;
+        if(left != null)
+            currentMax = Math.max(currentMax, left.getMax());
+        if(right != null)
+            currentMax = Math.max(currentMax, right.getMax());
+        return currentMax;
+    }
+
+    // O(n)
+    public ArrayList<Integer> getElementsAtLevel(int level){
+        return getElementsAtLevel(level, 0);
+    }
+
+    // O(n)
+    protected ArrayList<Integer> getElementsAtLevel(int level, int currentLevel){
+        if(level == currentLevel)
+            return new ArrayList<Integer>(value);
+        var elements = new ArrayList<Integer>();
+        if(left != null)
+            elements.addAll(left.getElementsAtLevel(level, currentLevel + 1));
+        if(right != null)
+            elements.addAll(right.getElementsAtLevel(level, currentLevel + 1));
+        return elements;
+    }
+
+
 
 }
