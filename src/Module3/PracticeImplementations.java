b5ed0b65835e266005398437f5c7d38d3eb3ce07
German De Francesco <germandefrancesco@hotmail.com>
2022-05-22 21:49:35 -0300

    implemented practice 3 exercise 2 part 2

--- src/Module3/PracticeImplementations.java
+++ src/Module3/PracticeImplementations.java
@@ -1,6 +1,8 @@
 package Module3;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class PracticeImplementations {
 
@@ -17,7 +19,7 @@ public class PracticeImplementations {
         }
     }
 
-    public static <T> int DFSVisit(Graph<T> graph, String[] vertexesVisited, int vertex, int time){
+    private static <T> int DFSVisit(Graph<T> graph, String[] vertexesVisited, int vertex, int time){
         vertexesVisited[vertex] = "AMARILLO";
         time++;
         var i = graph.getAdjacent(vertex);
@@ -29,6 +31,33 @@ public class PracticeImplementations {
         vertexesVisited[vertex] = "NEGRO";
         time++;
         return time;
+    }
+
+    public static <T> void BFS(Graph<T> graph){
+        var vertexesVisited = new boolean[graph.getVertexesAmount()];
+        var i = graph.getVertexes();
+        while (i.hasNext()){
+            var vertex = i.next();
+            if (!vertexesVisited[vertex])
+                BFSVisit(graph, vertexesVisited, vertex);
+        }
+    }
+
+    private static <T> void BFSVisit(Graph<T> graph, boolean[] vertexesVisited, int vertex){
+        vertexesVisited[vertex] = true;
+        var queue = new LinkedList<Integer>();
+        queue.addLast(vertex);
+        while (!queue.isEmpty()){
+            var vertexFromQueue = queue.removeFirst();
+            var i = graph.getAdjacent(vertexFromQueue);
+            while (i.hasNext()) {
+                int adjacentVertex = i.next();
+                if (!vertexesVisited[vertex]){
+                    vertexesVisited[vertex] = true;
+                    queue.addLast(adjacentVertex);
+                }
+            }
+        }
     }
 
     public static <T> boolean GraphHasCycle(Graph<T> graph){
@@ -45,7 +74,7 @@ public class PracticeImplementations {
         return false;
     }
 
-    public static <T> boolean GraphHasCycleVisit(Graph<T> graph, String[] vertexesVisited, int vertex){
+    private static <T> boolean GraphHasCycleVisit(Graph<T> graph, String[] vertexesVisited, int vertex){
         vertexesVisited[vertex] = "AMARILLO";
         var i = graph.getAdjacent(vertex);
         while (i.hasNext()) {
